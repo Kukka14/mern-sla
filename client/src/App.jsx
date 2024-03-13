21e0cba0d7a183bb004ce35725a421666ebe3f36
oushadhee <oushadhee0209@gmail.com>
2024-03-13 05:47:04 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-
+import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
  
 
@@ -12,7 +12,7 @@ export default function App() {
       
       <Routes>
         <Route path="/" element={<Home />} />
-       
+        <Route path="/signin" element={<SignIn/>}/>
         <Route path="/signup" element={<SignUp />} />
         
        
