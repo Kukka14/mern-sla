8ec09621933a13d269a804a3ca2e1748ef04f544
Maheesha <maheeu2000@gmail.com>
2024-03-14 18:57:40 +0530

    create contactus api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js";
+
+export const contactus = async (req, res) => {
+    const { name, email, contactNo, message } = req.body;
+
+    if (!name || !email || !contactNo || !message || name === '' || email === '' || contactNo === '' || message === '') {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+
+    const newUser = new User ({
+        name,
+        email,
+        contactNo,
+        message,
+    });
+
+    try {
+        await newUser.save();
+        res.json( 'Inquiary successful' );
+    }catch (error) {
+        res.status(500).json({message: error.message});
+    }
+    
+};
---
+++ api/routes/auth.route.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { contactus } from '../controllers/auth.controller.js';
+
+const router = express.Router();
+
+router.post('/contactus', contactus);
+
+export default router;
+
