deabca83e19c32ad39ef67c63b51e13cb23d34a0
oushadhee <oushadhee0209@gmail.com>
2024-03-17 18:53:41 +0530

    create signup api route

--- client/api/controllers/auth.controller.js
+++ client/api/controllers/auth.controller.js
@@ -1,3 +1,22 @@
+import User from "../models/user.model.js";
+
 export const signup = async (req,res) => {
-    console.log(req.body);
-}
+    
+    const{username,email,password} = req.body;
+
+    if(!username || !email || !password || username === '' || email === '' || password ==='')
+    {
+        return res.status(400).json({ message :'All field are required'});
+    }
+    const newUser = new User({
+        username,
+        email,
+        password,
+    });
+    try {
+        await newUser.save();
+        res.json('Signup successful');
+      } catch (error) {
+        next(error);
+      }
+    };
