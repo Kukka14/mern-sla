3e4bc985760a6af94351a16f5d506bb217dcb99f
oushadhee <oushadhee0209@gmail.com>
2024-03-17 17:22:12 +0530

    connect to the database

--- client/api/index.js
+++ client/api/index.js
@@ -2,22 +2,26 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'; // Corrected import name
 
 dotenv.config();
 
 mongoose
-.connect(process.env.MONGO)
-.then(() => { 
-    console.log('MongoDb is connected');
-})
-.catch((err) => {
-    console.log(err);
+  .connect(
+    process.env.MONGO)
+    .then(
+    () => {console.log('MongoDb is connected');
+}).catch(err =>{
+  console.log(err);
 });
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () =>{
-    console.log('Server is runnig on port 3000!!');
+  console.log('Server is running on port 3000!!');
 });
 
-app.use('/api/user',userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRouter);
