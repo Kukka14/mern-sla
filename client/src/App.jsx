16e4d43cb90e197f0d0cf02577b665ea9a8b1e81
Uvindu2002 <wupu0327@gmail.com>
2024-03-17 22:21:36 +0530

    Update navigation bar linking Other Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,9 @@ import SignUp from './pages/SignUp';
 import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import Product from './pages/Product';
+import Services from './pages/Services';
+import Services from './pages/Contact';
 
 export default function App() {
   return (
@@ -16,6 +19,9 @@ export default function App() {
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/about' element={<About/>} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/product' element={<Product />} />
+        <Route path='/Services' element={<Services />} />
+        <Route path='/contact' element={<Contact />} />
       </Routes>
     </BrowserRouter>
   );
