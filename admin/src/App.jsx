3470b8568981163cc26d0fa616bf339fd4528535
Uvindu2002 <wupu0327@gmail.com>
2024-03-19 05:50:11 +0530

    Main admin dashboard UI

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainDashboard from './MainDashboard';
 
 export default function App() {
   return (
-    <div className='text-red-500'>Admin</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainDashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        white: '#FFFFFF',
+        black: '#000000',
+        'sideNavBackground': '#ECFFEB',
+        'sideNavButton': '#2A491D',
+        'sideNavText': '#485A41',
+        'searchBarBackground': '#D9D9D9',
+        'sectionBackground': '#D3FFC1',
+        'sectionhr': '#7C9074',
+        'sectiontext': '#4C6653',
+    
+      },
+    },
   },
   plugins: [],
 }
