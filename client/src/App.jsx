ff2a51b5fb9df549a915ce009a5b8f5f97788326
kukka14 <thakshilagunawardena16@gmail.com>
2024-04-16 14:44:23 +0530

    separated client and admin pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import Products from './pages/Products';
-import Services from './pages/Services';
-import Contact from './pages/Contact';
+
+import Home from './pages/client/Home';
+import SignUp from './pages/client/SignUp';
+import About from './pages/client/About';
+import Profile from './pages/client/Profile';
+import Products from './pages/client/Products';
+import Services from './pages/client/Services';
+import Contact from './pages/client/Contact';
 import Header from './components/Header';
 
-export default function App() {    
+export default function App() {
   return (
-    <BrowserRouter>
-      <Header/>
+    <BrowserRouter> 
+      <Header/>     
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-in' element={< SignIn/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/sign-up' element={< SignUp/>} />
-        <Route path='/about' element={<About/>} />
+        <Route path='/about' element={< About/>} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/products' element={<Products/>} />
-        <Route path='/Services' element={<Services />} />
+        <Route path='/services' element={<Services />} />
         <Route path='/contact' element={<Contact />} />       
       </Routes>
     </BrowserRouter>
   );
 }
+
+
+
 
