8fbab87d0e0ba486a556143a91d60fb3919a35ec
oushadhee <oushadhee0209@gmail.com>
2024-04-18 03:23:46 +0530

    create sign up api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,27 @@
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+//import { errorHandler } from '../utils/error.js';
+//import jwt from 'jsonwebtoken';
+
+
+export const signup = async (req,res,next) =>{
+    const {username,email,password,nic,phoneNumber,address} =req.body;
+    const hashedPassword = bcryptjs.hashSync(password,10);
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword ,
+        nic,
+        phoneNumber,
+        address
+    });
+    try {
+        await newUser.save();
+        res.status(201).json("user created successfully");
+    } catch (error) {
+       next(error);
+    }
+    
+};
+
+
--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express, { Router } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(()=>{
@@ -16,3 +18,6 @@ app.use(express.json());
 app.listen(3000, ()=> {
     console.log('Server is running on port 3000');
 }); 
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
