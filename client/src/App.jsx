b2b7899a359870c4c0bfd11eb39546f40e5d1a41
kukka14 <thakshilagunawardena16@gmail.com>
2024-04-20 02:46:52 +0530

    update admin product listning funtionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,16 +7,9 @@ import Header from './components/Header.jsx';
 import Profile from './pages/client/Profile.jsx';
 import CustomerManagement from './pages/admin/MProfile.jsx';
 import PrivateRoute from './components/PrivateRoute';
-<<<<<<< HEAD
 import ProductListing from './pages/admin/ProductListing.jsx';
-
-
-
-
-=======
 import AddEmployee from './pages/admin/AddEmployee.jsx';
 import MainDashboard from './pages/admin/MainDashboard.jsx';
->>>>>>> main
 
 export default function App() {
   const { currentUser } = useSelector((state) => state.user);
@@ -26,31 +19,18 @@ export default function App() {
     <BrowserRouter>
       {isAdmin ? null : <Header />}
       <Routes>
-<<<<<<< HEAD
         <Route path='/' element={<Home/>} />
         <Route path='product-listing' element={<ProductListing/>} />
         
         <Route path='/sign-up' element={<SignUp />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path="/customer-management" element={<CustomerManagement />} /> 
-         
-       
-        <Route element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile />} /> 
-=======
-        <Route path='/' element={<Home />} />
-        <Route path='/sign-up' element={<SignUp />} />
-        <Route path='/sign-in' element={<SignIn />} />
-        <Route path="/customer-management" element={<CustomerManagement />} />
         
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/mainDashboard' element={<MainDashboard />} />
           <Route path='/addEmployee' element={<AddEmployee />} />
         </Route>
-        
-        
->>>>>>> main
         
       </Routes>
     </BrowserRouter>
