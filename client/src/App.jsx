c80f46537261b79566b75cb94017803c08f5f19e
Uvindu2002 <wupu0327@gmail.com>
2024-04-19 18:07:15 +0530

    Add add employee functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Header from './components/Header.jsx';
 import Profile from './pages/client/Profile.jsx';
 import CustomerManagement from './pages/admin/MProfile.jsx'
 import PrivateRoute from './components/PrivateRoute';
+import AddEmployee from './pages/admin/AddEmployee.jsx';
+import MainDashboard from './pages/admin/MainDashboard.jsx';
 
 
 
@@ -22,9 +24,10 @@ export default function App() {
          <Route path="/customer-management" element={<CustomerManagement />} /> 
        
         <Route element={<PrivateRoute />}>
-        <Route path='/profile' element={<Profile />} /> 
-        
-        </Route>    
+          <Route path='/profile' element={<Profile />} />       
+        </Route>
+        <Route path='/mainDashboard' element={<MainDashboard />} /> 
+        <Route path='/addEmployee' element={<AddEmployee />} /> 
       </Routes>
     </BrowserRouter>
   );
