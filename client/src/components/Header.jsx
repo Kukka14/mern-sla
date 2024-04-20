b6011f3ff44275cea7b543202cf7d56b0ec91cdf
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-04-20 19:04:07 +0530

    Update package-lock.json and add react-rating-stars-component dependency

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,9 @@
 
-<<<<<<< HEAD
+
 import  { useState } from 'react';
-=======
-import { FaSearch } from 'react-icons/fa';
->>>>>>> main
+
+
+
 import { Link } from 'react-router-dom';
 import logo1 from '../images/companylogo1.png';
 import { useSelector } from 'react-redux';
@@ -51,8 +51,8 @@ export default function Header() {
           <div className='flex flex-row justify-between'>
             <div className='bg-backgreen3 w-12 h-12 flex justify-center items-center rounded-full mr-7'>
             {currentUser ? (
-              <div className="relative">
-              <button onClick={handleToggleCart} className="mr-4 text-white focus:outline-none">
+<div className='relative'>
+              <button onClick={handleToggleCart} className=" text-white focus:outline-none">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 text-white">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
               </svg>
