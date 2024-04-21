9fba8bc7379c4c0aa2cbd2423aab1bb9f8f81947
Uvindu2002 <wupu0327@gmail.com>
2024-04-21 21:48:32 +0530

    Create add Category Functionality

---
+++ api/models/category.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const categorySchema = new mongoose.Schema({
+    categoryname: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+
+    description: {
+        type: String,
+        required: true,
+    },
+
+    avatar: {
+        type: String, 
+        required: true,
+    }
+
+    
+}, {timestamps: true});
+
+const Category = mongoose.model('Category', categorySchema);
+
+export default Category;
