1ea3cca77f24003ee9bc2dc74e97a0f7822b1150
kukka14 <thakshilagunawardena16@gmail.com>
2024-05-02 17:37:57 +0530

    some package update

--- client/src/pages/admin/ProductManagement/ProductAdminDashboard.jsx
+++ client/src/pages/admin/ProductManagement/ProductAdminDashboard.jsx
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
-import logo from './../../images/logo2.png';
-import dashboard from './../../images/icons8-arrow-50 (1).png';
+import logo from '../../../images/logo2.png';
+import dashboard from '../../../images/icons8-arrow-50 (1).png';
 import { FaSortAmountDown } from 'react-icons/fa';
-import AdminHeader from '../../components/AdminHeader';
+import AdminHeader from '../../../components/AdminHeader';
+
 
 export default function MainDashboard() {
   return (
