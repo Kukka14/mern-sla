0f01b83075dc16513044ddca04c5c056c85e7907
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-05-09 14:32:17 +0530

    feat: Optimize order retrieval by sorting new and all orders by creation date

--- client/src/pages/client/Ordersummary.jsx
+++ client/src/pages/client/Ordersummary.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
-import axios from "axios";
 
 export default function Ordersummary() {
   const [orderDetails, setOrderDetails] = useState({});
