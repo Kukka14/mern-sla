05df1b342ec159905fcc7c40c91a86d1b1a396ca
DulinaSH2001 <dulinahejithaindrawansha@gmail.com>
2024-05-14 05:09:55 +0530

    chore: Update import statement in OrderDashboard.jsx

--- client/src/pages/admin/OrderManagement/OrderDashboard.jsx
+++ client/src/pages/admin/OrderManagement/OrderDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Chart from "chart.js/auto";
 
@@ -14,7 +14,7 @@ export default function MainDashboard() {
 
   useEffect(() => {
     fetchOrderCounts();
-  }, []);
+  }, [ ]);
 
   useEffect(() => {
     if (chartInstance) {
